01014e414dd98f4709e63a289900a7d293ec694e
Pavle <Pavle.Svrdlin@city.ac.uk>
2021-12-13 16:12:56 +0000

    3 new methods created to print output to the user

--- src/main/java/sort_manager/managers/OutputManager.java
+++ src/main/java/sort_manager/managers/OutputManager.java
@@ -3,4 +3,22 @@ package sort_manager.managers;
 // Class that manages any output
 
 public class OutputManager {
+    public static void printIntroMsg() {
+        System.out.println("Welcome to Sort Manager!");
+        System.out.println("Please enter the number of the sorter you wish to use:");
+        System.out.printf("1. Binary Tree Sorter%n" +
+                "2. Bubble Sorter%n" +
+                "3. Merge Sorter");
+    }
+
+    public static void printArrayMsg() {
+        System.out.println("Please enter the size of array you wish to generate: ");
+    }
+
+    public static void printResults() {
+        System.out.println("Sorting using the ");
+        System.out.println("Before sorting: ");
+        System.out.println("After sorting: ");
+        System.out.println("Time taken: ");
+    }
 }
