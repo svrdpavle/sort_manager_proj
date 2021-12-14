b9b96edc54c9eb3c89cab3b68bc81be6a5d534ae
Pavle <Pavle.Svrdlin@city.ac.uk>
2021-12-14 00:11:49 +0000

    created a method to generate an array of random integers

--- src/main/java/sort_manager/managers/ArrayManager.java
+++ src/main/java/sort_manager/managers/ArrayManager.java
@@ -1,4 +1,15 @@
 package sort_manager.managers;
 
+import java.util.Random;
+
 public class ArrayManager {
+    // method to generate a random array
+    public static int[] randomArray(int size) {
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (rand.nextInt(size) + 1);
+        }
+        return arr;
+    }
 }
