197bc876e1634aa67f4de933dbea5accec3880d1
Pavle <Pavle.Svrdlin@city.ac.uk>
2021-12-15 15:55:49 +0000

    bubblesort functionality implemented

--- src/main/java/sort_manager/sorters/BubbleSort.java
+++ src/main/java/sort_manager/sorters/BubbleSort.java
@@ -10,10 +10,25 @@ public class BubbleSort implements Sorter {
 
     // bubble sort algorithm
     private int[] bubbleSort(int[] generatedArr) {
-        int[] bubbleSortArr = new int[generatedArr.length];
-        for (int i = 0; i < generatedArr.length; i++) {
-            bubbleSortArr[i] = generatedArr[i];
+        int[] bubbleSortArr = transferArray(generatedArr);
+        int temp = 0;
+        for (int i = 0; i < bubbleSortArr.length; i++) {
+            for (int j = 1; j < ((bubbleSortArr.length) - i); j++) {
+                if (bubbleSortArr[j-1] > bubbleSortArr[j]) {
+                    temp = bubbleSortArr[j-1];
+                    bubbleSortArr[j-1] = bubbleSortArr[j];
+                    bubbleSortArr[j] = temp;
+                }
+            }
         }
         return bubbleSortArr;
+    }
+
+    private int[] transferArray(int[] arr1) {
+        int[] arr2 = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+        return arr2;
     }
 }
