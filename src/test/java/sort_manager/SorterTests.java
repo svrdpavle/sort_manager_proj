9514157dd9487fc4db54c4ba5b4947fb26f13c17
Pavle <Pavle.Svrdlin@city.ac.uk>
2021-12-17 16:36:06 +0000

    6 tests run for each sorter class

--- src/test/java/sort_manager/SorterTests.java
+++ src/test/java/sort_manager/SorterTests.java
@@ -7,22 +7,36 @@ import sort_manager.sorters.Sorter;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SorterTests {
     Sorter binaryTreeSorter = SortFactory.sorterFactory("Binary Tree");
     Sorter bubbleSorter = SortFactory.sorterFactory("Bubble");
     Sorter mergeSorter = SortFactory.sorterFactory("Merge");
+
     int[] largeUnsortedArr = ArrayHandler.randomArray(10000);
+
     int[] positiveArr = {1, 7, 4, 6, 3, 9, 2, 8, 10, 5};
     int[] positiveArrSorted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    int[] negativeArr = {-1, -7, -4, -6, -3, -9, -2, -8, -10, -5};
+    int[] negativeArrSorted = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1};
+
+    int[] alreadySortedArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int[] oneElementArr = {1};
+
+    int[] oddSizedArr = {2, 5, 3, 1, 4};
+    int[] oddSizedArrSorted = {1, 2, 3, 4, 5};
+
     @BeforeAll
     static void beforeAll(TestInfo testInfo) {
-        System.out.println(testInfo.getDisplayName() + " executing.");
+        System.out.println("<" + testInfo.getDisplayName() + "> executing");
     }
 
     @BeforeEach
     void setUp(TestInfo testInfo) {
-        System.out.println(testInfo.getDisplayName() + " is being tested");
+        System.out.println("<" + testInfo.getDisplayName() + "> is being tested");
     }
 
     // Binary Tree sorter tests
@@ -34,13 +48,37 @@ public class SorterTests {
         void sortsLargeArr() {
             int[] myBinaryTreeSortedArr = binaryTreeSorter.sortArray(largeUnsortedArr);
             Arrays.sort(largeUnsortedArr);
-            Assertions.assertArrayEquals(largeUnsortedArr, myBinaryTreeSortedArr);
+            assertArrayEquals(largeUnsortedArr, myBinaryTreeSortedArr);
         }
 
         @Test
-        @DisplayName("Sorts positive array of ints")
-        void sortsPositiveArrOfInts() {
-            Assertions.assertArrayEquals(positiveArrSorted, binaryTreeSorter.sortArray(positiveArr));
+        @DisplayName("Sorts positive array")
+        void sortsPositiveArr() {
+            assertArrayEquals(positiveArrSorted, binaryTreeSorter.sortArray(positiveArr));
+        }
+
+        @Test
+        @DisplayName("Sorts negative array")
+        void sortsNegativeArr() {
+            assertArrayEquals(negativeArrSorted, binaryTreeSorter.sortArray(negativeArr));
+        }
+
+        @Test
+        @DisplayName("Sorts a sorted array")
+        void sortsSortedArr() {
+            assertArrayEquals(alreadySortedArr, binaryTreeSorter.sortArray(alreadySortedArr));
+        }
+
+        @Test
+        @DisplayName("Sorts a one element array")
+        void sortsOneElementArr() {
+            assertArrayEquals(oneElementArr, binaryTreeSorter.sortArray(oneElementArr));
+        }
+
+        @Test
+        @DisplayName("Sorts a odd sized array")
+        void sortsOddSizedArr() {
+            assertArrayEquals(oddSizedArrSorted, binaryTreeSorter.sortArray(oddSizedArr));
         }
     }
 
@@ -53,13 +91,37 @@ public class SorterTests {
         void sortsLargeArr() {
             int[] myBubbleSortedArr = bubbleSorter.sortArray(largeUnsortedArr);
             Arrays.sort(largeUnsortedArr);
-            Assertions.assertArrayEquals(largeUnsortedArr, myBubbleSortedArr);
+            assertArrayEquals(largeUnsortedArr, myBubbleSortedArr);
         }
 
         @Test
-        @DisplayName("Sorts positive array of ints")
-        void sortsPositiveArrOfInts() {
-            Assertions.assertArrayEquals(positiveArrSorted, bubbleSorter.sortArray(positiveArr));
+        @DisplayName("Sorts positive array")
+        void sortsPositiveArr() {
+            assertArrayEquals(positiveArrSorted, bubbleSorter.sortArray(positiveArr));
+        }
+
+        @Test
+        @DisplayName("Sorts negative array")
+        void sortsNegativeArr() {
+            assertArrayEquals(negativeArrSorted, bubbleSorter.sortArray(negativeArr));
+        }
+
+        @Test
+        @DisplayName("Sorts an already sorted array")
+        void sortsSortedArr() {
+            assertArrayEquals(alreadySortedArr, bubbleSorter.sortArray(alreadySortedArr));
+        }
+
+        @Test
+        @DisplayName("Sorts a one element array")
+        void sortsOneElementArr() {
+            assertArrayEquals(oneElementArr, bubbleSorter.sortArray(oneElementArr));
+        }
+
+        @Test
+        @DisplayName("Sorts a odd sized array")
+        void sortsOddSizedArr() {
+            assertArrayEquals(oddSizedArrSorted, bubbleSorter.sortArray(oddSizedArr));
         }
     }
 
@@ -72,23 +134,47 @@ public class SorterTests {
         void sortsLargeArr() {
             int[] myMergeSortedArr = mergeSorter.sortArray(largeUnsortedArr);
             Arrays.sort(largeUnsortedArr);
-            Assertions.assertArrayEquals(largeUnsortedArr, myMergeSortedArr);
+            assertArrayEquals(largeUnsortedArr, myMergeSortedArr);
         }
 
         @Test
-        @DisplayName("Sorts positive array of ints")
-        void sortsPositiveArrOfInts() {
-            Assertions.assertArrayEquals(positiveArrSorted, mergeSorter.sortArray(positiveArr));
+        @DisplayName("Sorts positive array")
+        void sortsPositiveArr() {
+            assertArrayEquals(positiveArrSorted, mergeSorter.sortArray(positiveArr));
+        }
+
+        @Test
+        @DisplayName("Sorts negative array")
+        void sortsNegativeArr() {
+            assertArrayEquals(negativeArrSorted, mergeSorter.sortArray(negativeArr));
+        }
+
+        @Test
+        @DisplayName("Sorts an already sorted array")
+        void sortsSortedArr() {
+            assertArrayEquals(alreadySortedArr, mergeSorter.sortArray(alreadySortedArr));
+        }
+
+        @Test
+        @DisplayName("Sorts a one element array")
+        void sortsOneElementArr() {
+            assertArrayEquals(oneElementArr, mergeSorter.sortArray(oneElementArr));
+        }
+
+        @Test
+        @DisplayName("Sorts a odd sized array")
+        void sortsOddSizedArr() {
+            assertArrayEquals(oddSizedArrSorted, mergeSorter.sortArray(oddSizedArr));
         }
     }
 
     @AfterEach
     void tearDown(TestInfo testInfo) {
-        System.out.println(testInfo.getDisplayName() + " has finished testing");
+        System.out.println("<" + testInfo.getDisplayName() + "> has finished testing");
     }
 
     @AfterAll
     static void afterAll(TestInfo testInfo) {
-        System.out.println(testInfo.getDisplayName() + " completed");
+        System.out.println("<" + testInfo.getDisplayName() + "> completed");
     }
 }
